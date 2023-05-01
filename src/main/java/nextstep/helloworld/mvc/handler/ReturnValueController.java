24e2ad12eec144f1ec6a3cfefc7665615d2427fe
jiwon <ljwon77@daum.net>
2023-05-01 19:47:59 +0900

    feat: thymeleaf 템플릿 학습 테스트 코드 추가

--- src/main/java/nextstep/helloworld/mvc/handler/ReturnValueController.java
+++ src/main/java/nextstep/helloworld/mvc/handler/ReturnValueController.java
@@ -3,9 +3,11 @@ package nextstep.helloworld.mvc.handler;
 import nextstep.helloworld.mvc.domain.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -36,7 +38,8 @@ public class ReturnValueController {
     }
 
     @GetMapping("/thymeleaf")
-    public String thymeleaf() {
+    public String thymeleaf(final Model model) {
+        model.addAttribute("name", "져니");
         return "sample";
     }
 }
